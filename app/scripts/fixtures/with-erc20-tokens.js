726d3299307b0ca1775030f8162759bb753c7255
ericapacker194 <153939118+ericapacker194@users.noreply.github.com>
2025-07-15 00:46:11 -0600

    Create with-erc20-tokens.js

---
+++ app/scripts/fixtures/with-erc20-tokens.js
@@ -0,0 +1,10 @@
+const _TOKENS = [
+  { address: '0x6B175474E89094C44Da98b954EedeAC495271d0F', symbol: 'DAI', decimals: 18, image: 'https://static.metafi.codefi.network/api/v1/tokenIcons/1/0x6b175474e89094c...', isERC721: false, aggregators: [] },
+  { address: '0xA0b86991c6218b36c1d19D...', symbol: 'USDC', decimals: 6, image: 'https://static.metafi.codefi.network/api/v...efacefeb.png', isERC72.. aggregators = [] },
+];
+
+export const FIXTURES_ERC20_TOKENS = {
+  tokens:_TOKENS,
+  ignoredTokens:[], detectedTokens:[], allTokens:{'':{myAccount:_TOKENS}},
+allIgnoredTokens:{}, allDetectedTokens:{}
+};
