b0e1dbfffe93b022a2bff4bda948949f3402b0e5
ericapacker194 <153939118+ericapacker194@users.noreply.github.com>
2025-07-15 00:41:24 -0600

    Create import-srp.stories.tsx

---
+++ ui/pages/multi-srp/import-srp/import-srp.stories.tsx
@@ -0,0 +1,6 @@
+ ```
+import React from 'react';
+import { ImportSrp } from '.';
+export default { title: 'Components/Multichain/ImportSrp', component: ImportSrp };
+export const DefaultStory = (args) => <ImportSrp {...args} />;
+DefaultStory.storyName = 'Default';
